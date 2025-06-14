bb33627a36f3a58c53a34cb3e7b0cf75096d3963
karthika-2709 <vadlakarthika1@gmail.com>
2025-06-14 21:25:22 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,3 +1,18 @@
 # merging-two-sorted-arrays
+def merge(nums1,nums2)):
+  i=0
+  j=0
+  arr=[]
+  while(i<=nums1 and j<=nums2):
+    if(nums[i]==nums[j]):
+      arr.append(nums[i])
+      i+=1
+    if(nums[i]<nums[j]):
+      arr.append(nums[i])
+      i+=1
+    else:
+      arr.append(nums[j])
+      j+=1
 nums1=list(map(int,input().split())
-nums
+nums2=list(map(int,input().split())
+print(merge(nums1,nums2))
